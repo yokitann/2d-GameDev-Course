581ce66fa50ab7d2e11cec406f7a20a96a577900
yokitann <yotan3@s.sfusd.edu>
2024-03-15 12:25:31 -0700

    L5

--- 04.to_space_and_beyond_workbook/ship.gd
+++ 04.to_space_and_beyond_workbook/ship.gd
@@ -4,17 +4,21 @@ var max_speed := 600.0
 var velocity := Vector2(0, 0)
 var boost_speed = 1500.0
 var normal_speed = 600.0
+var steering_factor := 10.0
+
 
 func _process(delta: float) -> void:
 	var direction := Vector2(0, 0)
 	direction.x = Input.get_axis("move_left", "move_right")
 	direction.y = Input.get_axis("move_up", "move_down")
+	var desired_velocity := max_speed * direction
+	var steering_vector := desired_velocity - velocity
 	
 	if Input.is_action_just_pressed("boost"):
 		max_speed = boost_speed
 		get_node("Timer").start()
 	
-	velocity = direction * max_speed 
+	velocity += steering_vector * steering_factor * delta
 	position += velocity * delta
 	
 	if direction.length() > 0.0:
